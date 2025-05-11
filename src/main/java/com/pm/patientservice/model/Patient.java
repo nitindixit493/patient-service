266ce5cd8dfbfbdd972077a2d6bfff7a496a9407
Prashant701790 <prashant.upadhyay@aqad.ae>
2025-05-11 12:30:45 +0530

    First Commit

---
+++ src/main/java/com/pm/patientservice/model/Patient.java
@@ -0,0 +1,27 @@
+package com.pm.patientservice.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+
+import java.util.UUID;
+
+@Entity
+public class Patient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID patientId;
+
+    @NotNull
+    private String name;
+
+
+
+
+
+
+
+}
